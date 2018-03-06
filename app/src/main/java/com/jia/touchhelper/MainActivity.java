d44ad3d5b9ca15f72450d279be7b525f18cccf75
shuaijia <819418850@qq.com>
2018-03-06 09:52:41 +0800

    jia:

--- app/src/main/java/com/jia/touchhelper/MainActivity.java
+++ app/src/main/java/com/jia/touchhelper/MainActivity.java
@@ -1,13 +1,29 @@
 package com.jia.touchhelper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_edit)
+    TextView tvEdit;
+    @BindView(R.id.rv_mine)
+    RecyclerView rvMine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_edit)
+    public void onViewClicked() {
     }
 }
